feecb32714911db73a97cc00ac733de59e087dd0
Napier <matsumaru.t.aa@m.titech.ac.jp>
2020-05-09 17:20:24 +0900

    First day commit

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("東京工業大学")
+                    .font(.title)
+                HStack {
+                    Text("Tokyo Institute of Technology")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("東京都目黒区大岡山")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 11")
+            ContentView()
+                .previewDevice("iPhone 8")
+        }
     }
 }
